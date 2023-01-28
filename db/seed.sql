c0a6ec8b3bc63619b1d0be30926259c4909f593e
MaddieGillis <112782694+MaddieGillis@users.noreply.github.com>
2023-01-27 16:59:16 -0800

    Filled in seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,27 @@
+USE employee_db;
+
 INSERT INTO department(name)
 VALUES ("Engineering"),
         ("Finance"),
         ("Legal"),
         ("Sales");
-        
-INSERT INTO role (title, salary)
+
+USE employee_db;
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Software Engineer", 80000, 1),
+        ("Accountant", 800000, 2),
+        ("Lawyer", 800000, 3),
+        ("Sales Representative", 800000, 4);
+
+USE employee_db;
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUE ("Haylee", "Stevenson", 1, NULL),
+        ("Kristina", "Smith", 1, 1),
+        ("Spencer", "Davis", 2, NULL),
+        ("Barbra", "MacDonald", 2, 3),
+        ("Shayne", "Barlowe", 3, NULL),
+        ("Miranda", "Katz", 3, 5),
+        ("Brian", "Elrod", 4, NULL),
+        ("Hank", "Piker", 4, 7);
